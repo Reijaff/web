cdff53382b27b81de6d5d342033111910c3d736e
Reijaff <interlord406@gmail.com>
2019-08-14 18:04:20 +0300

    ...

--- bittorrent_client/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs'),
-    bencode = require('bencode');
-
-const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-
-console.log(torrent.announce.toString('utf8'));
