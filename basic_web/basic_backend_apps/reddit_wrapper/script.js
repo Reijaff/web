bd47cbc32c4d71ccd103de327f19a3c9fdde6f87
Reijaff <interlord406@gmail.com>
2019-08-21 01:23:33 +0300

    subreddit parser update

--- basic_web/basic_backend_apps/reddit_wrapper/script.js
+++ basic_web/basic_backend_apps/reddit_wrapper/script.js
@@ -1,5 +1,33 @@
-const fetch = require('node-fetch');
-const url = "https://www.reddit.com/search.json?q=haha";
-fetch(url)
-    .then(res => res.json())
-    .then(body => console.log(body.data.children[0]));
+#!/usr/bin/env node
+
+var rawjs = require('raw.js');
+var reddit = new rawjs('myWrapper001');
+var program = require('commander');
+
+reddit.setupOAuth2('zE9b5C3dYs4MQQ', 'R5RaFWm7n_zwdCvh1PlMwxhTNhw');
+
+program
+    .option(
+        '-r, --subreddit <sub>',
+        'Specify a subreddit to parse',
+        'programming',
+    )
+    .option(
+        '-l, --limit <num>',
+        'Specify the maximum number of results to return. Default 25, maximum 100',
+        25,
+    );
+program.parse(process.argv);
+
+reddit.hot({r: program.subreddit, limit: program.limit}, (err, res) => {
+    var count = 0;
+    console.log('subreddit : r/' + program.subreddit + "\n");
+    for (let val of res.children) {
+        console.log(
+            '%d.%s\ncontent: %s\n',
+            ++count,
+            val.data.title,
+            val.data.url,
+        );
+    }
+});
